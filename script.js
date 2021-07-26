b7f6ff205e000d21294d87be472c686791ef5a18
Brenno Cavalcante <martiniano.brenno@gmail.com>
2021-07-26 17:36:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+    let input = document.querySelector('#searchInput').value
+    
+    if(input !== ''){
+        clearInfo()
+        showWarning('Carregando...')
+        //encode serve para remover os espaços em branco em caracteres especiais
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=b9f926013d032fc122a244fa591cbdd1&units=metric&lang=pt_br` 
+        
+        //fazendo requisição e obtendo resultado
+        let results = await fetch(url) 
+        let json = await results.json() //transforma o resultado em json
+
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        } else {
+            clearInfo()
+            showWarning('Não encontramos esta localização')
+        }
+
+    } else {
+        clearInfo()
+    }
+
+})
+
+//exibindo informações nos respectivos icones
+function showInfo(json){
+    showWarning('')
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>°C</sup>`
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`
+    
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`
+    
+    document.querySelector('.resultado').style.display = 'block' //teste
+    
+}
+
+//limpando a tela
+function clearInfo(){
+    showWarning('')
+    document.querySelector('.resultado').style.display = 'none'
+}
+
+//exibindo mensagem
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg
+}
